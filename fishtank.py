57dd91c755e7f9d01c896b192a6ac0b63fddaa5f
NightShiftKing <michaelmonreal482@gmail.com>
2025-03-18 15:25:05 -0600

    added bubbles

--- fishtank.py
+++ fishtank.py
@@ -2,14 +2,34 @@ import pygame
 import random
 import time
 import button
+import math
 
 
 
 pygame.init()
-screen = pygame.display.set_mode((800,600))
+screenHeight = 600
+screenWidth = 800
+screen = pygame.display.set_mode((screenWidth,screenHeight))
 pygame.display.set_caption("Fish Simulator")
 clock = pygame.time.Clock() 
 count = 1
+
+class Bubbles:
+    def __init__(self, x, y):
+        self.xpos = x
+        self.ypos = y
+        self.color = (random.randrange(50,200), random.randrange(100,255), random.randrange(100,255))
+    def move(self):
+        self.xpos += random.randrange(-2, 3)
+        self.ypos -= random.randrange(0, 3)
+        if self.ypos <0:
+            self.ypos = 500
+    def PosToRad(self):
+        num = ((self.ypos*math.pi)/1000 +1) - 2*math.pi
+        size = 10*math.sin(num)
+        return size
+    def draw(self):
+        pygame.draw.circle(screen, self.color, (self.xpos, self.ypos), self.PosToRad(), 2) #draw flakes
 
 class Fish:
     def __init__(self):
@@ -60,6 +80,9 @@ addbutton= button.Button(600,200,20,20,(255,255,255), 'hello')
 
 # player variables
 
+bubble = []
+for i in range(500):
+    bubble.append(Bubbles(random.randrange(0,screenWidth), random.randrange(0,screenHeight)))
 
 fish = []
 for i in range(count): 
@@ -79,6 +102,9 @@ while running:# Game loop#######################################################
     mousePos = pygame.mouse.get_pos()
     AddFish(addbutton,count)
 
+    for i in range(len(bubble)):
+        bubble[i].move()
+
 
   
 
@@ -93,6 +119,9 @@ while running:# Game loop#######################################################
         fish[i].draw(screen)
         fish[i].move()
     addbutton.draw(screen)
+
+    for i in range(len(bubble)):
+        bubble[i].draw()
 
 
     # Update the display
